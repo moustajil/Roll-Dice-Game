05ac6899ac16f13471d6e064a037ef030658b246
Aimad Moustajil <moustajil.dev@gmail.com>
2025-05-03 10:51:21 +0100

    adding function random

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,25 +1,51 @@
-import 'package:flutter/material.dart';
-import 'package:rolldicegame/text_widget.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
 class HomeScreen extends StatefulWidget {
   List<Color> Colors;
-  HomeScreen({super.key,required this.Colors});
+  HomeScreen({super.key, required this.Colors});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var activeRollDice = "assets/images/dice-1.png";
+
+  void rollDice() {
+    setState(() {
+      int randomNumber = Random().nextInt(6) + 1;
+      activeRollDice = "assets/images/dice-$randomNumber.png";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: widget.Colors
+          gradient: LinearGradient(colors: widget.Colors),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(activeRollDice, width: 150),
+              SizedBox(height: 40),
+              TextButton(
+                onPressed: rollDice,
+                child: Text("Play"),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+
+                  textStyle: TextStyle(color: Colors.black, fontSize: 28),
+                ),
+              ),
+            ],
           ),
         ),
-        child: TextWidget(title: "Hello Word"),
       ),
     );
   }
